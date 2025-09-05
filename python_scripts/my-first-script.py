f804b8f9ac06f7acd30dc0430ce0e7e4228682ea
Andrew Alverson <aja@uark.edu>
2025-09-05 12:11:32 -0500

    first commit

---
+++ python_scripts/my-first-script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# get input from user
+name = input('give me your name, fool: ')
+
+print('pleasure to know you', name)
+
+x = input('give me a number: ')
+y = input('give me another number: ')
+
+x = int(x)
+y = int(y)
+
+# do some math
+print(x, y, x+y)
+print(type(x))
