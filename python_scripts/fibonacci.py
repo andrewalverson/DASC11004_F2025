1291c67ce432701e7c7c991379c7ecb07e1d2ee9
Andrew Alverson <aja@uark.edu>
2025-09-17 13:43:45 -0500

    added option for verbose output

--- python_scripts/fibonacci.py
+++ python_scripts/fibonacci.py
@@ -11,6 +11,14 @@ parser = argparse.ArgumentParser(description = "This script \
 # add our required (positional) argument
 parser.add_argument("position", help = "position in the Fibonacci sequence", type = int)
 
+# add optional arguments
+# if 'store_true', this means assign 'True' if the argument is specified
+# in the command line, so the default for 'store_true' is false
+parser.add_argument("-v", "--verbose", 
+	help = "whether to print verbose output, default = false", 
+	action = 'store_true')
+
+
 # parse the argument line
 args = parser.parse_args()
 
@@ -25,5 +33,7 @@ for i in range(int(args.position)):
 
 fibonacci_number = a
 
-print(f"The Fibonacci number for {args.position} is {fibonacci_number}")
-
+if args.verbose:
+	print(f"The Fibonacci number for {args.position} is {fibonacci_number}")
+else:
+	print(fibonacci_number)
