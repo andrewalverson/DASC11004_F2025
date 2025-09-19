f74427eaf5a112126a1553600e9d0bca80c5b784
Andrew Alverson <aja@uark.edu>
2025-09-19 13:30:41 -0500

    now uses argparse()

--- python_scripts/password-generator.py
+++ python_scripts/password-generator.py
@@ -4,18 +4,27 @@
 
 import random
 import string
+import argparse
+
+# create an arugment parser object
+parser = argparse.ArgumentParser(description = "This script \
+	generates a secure password of the desired length")
+
+# add the required (positional) argument, i.e., the password length
+parser.add_argument("pw_length", help = "Desired password length (integer)", type = int)
+
+# parse the arguments
+args = parser.parse_args()
 
 # create a variable to hold the password
 password = ''
 
-pw_length = input("Enter the password length: ")
-pw_length = int(pw_length)
 
 # a single string with all the possible password characters
 # lowercase are included twice so we get more of those
 characters = string.ascii_letters + string.ascii_lowercase + string.digits + string.punctuation
 
-for i in range(pw_length):
+for i in range(args.pw_length):
 	password += random.choice(characters)
 
 print(password)
